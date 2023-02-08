4e1cf839b682e446e86105d40ff5d1107864902e
LaetitiaBodin <laetitia.bodin.webdev@gmail.com>
2023-02-08 15:30:25 +0100

    feat : widget done (theme switcher)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'theme-switcher';
+
+  theme: string = "light";
+  pos: string = "";
+
+  goLight() {
+    const meta = document.querySelector('meta[name=theme-color]');
+    meta?.setAttribute('content', '#0b090a');
+    this.theme = "light";
+    return (this.pos = "switcher--light");
+  }
+
+  goDark() {
+    const meta = document.querySelector('meta[name=theme-color]');
+    meta?.setAttribute('content', '#e0afa0');
+    this.theme = "dark";
+    return (this.pos = "switcher--dark");
+  }
+  
 }
